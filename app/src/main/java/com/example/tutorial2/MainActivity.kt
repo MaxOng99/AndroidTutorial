fd8b698fa5387592387b9435f26dd849a19af28f
yco1g18 <yco1g18@soton.ac.uk>
2020-07-21 18:04:24 +0800

    feature/doc: add search icon to toolbar, change toolbar title

--- app/src/main/java/com/example/tutorial2/MainActivity.kt
+++ app/src/main/java/com/example/tutorial2/MainActivity.kt
@@ -2,12 +2,27 @@ package com.example.tutorial2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import androidx.appcompat.widget.Toolbar
+import kotlinx.android.synthetic.main.simple_toolbar.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setSupportActionBar(findViewById(R.id.simple_toolbar))
         setContentView(R.layout.e_doc)
-        supportActionBar?.title = "edoc"
+
+        var toolbar = findViewById<Toolbar>(R.id.e_doc_toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+
+        simple_toolbar_title.text = "edoc"
+        Log.d("option: ", "option menu")
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        Log.d("option: ", "option menu")
+        menuInflater.inflate(R.menu.menu_resource, menu)
+        return true
     }
 }
